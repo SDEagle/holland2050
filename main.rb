4f5d56b1a4244c617882993d719840bd4a428718
Tim 'S.D.Eagle' Zeitz <tim.zeitz@googlemail.com>
2013-05-31 01:49:17 +0200

    move actions

--- main.rb
+++ main.rb
@@ -45,10 +45,58 @@ class Game
   end
 
   def move_possible? bot, direction
+    possible_height = height @positions[bot] + 1
     case direction
       when MOVE_UP
-        @field[@positions[bot].x][@positions[bot].y+1]
+        possible_height >= @field[@positions[bot][0]][@positions[bot][1]+1]
+      when MOVE_LEFT
+        possible_height >= @field[@positions[bot][0]-1][@positions[bot][1]]
+      when MOVE_DOWN
+        possible_height >= @field[@positions[bot][0]][@positions[bot][1]-1]
+      when MOVE_RIGHT
+        possible_height >= @field[@positions[bot][0]+1][@positions[bot][1]]
+      else
+        false
     end
+  end
+
+  def move bot, direction
+    case direction
+      when MOVE_UP
+        @positions[bot][1] += 1
+      when MOVE_LEFT
+        @positions[bot][0] -= 1
+      when MOVE_DOWN
+        @positions[bot][1] -= 1
+      when MOVE_RIGHT
+        @positions[bot][0] += 1
+      else
+        false
+    end
+  end
+
+  def dig bot
+    @field[@positions[bot][0]][@positions[bot][1]] -= 1
+  end
+
+  def raise_possible bot
+    current_height = height @positions[bot]
+    height(@positions[bot] + Vector[1, 0]) >= current_height ||
+    height(@positions[bot] + Vector[1, -1]) >= current_height ||
+    height(@positions[bot] + Vector[0, -1]) >= current_height ||
+    height(@positions[bot] + Vector[-1, -1]) >= current_height ||
+    height(@positions[bot] + Vector[-1, 0]) >= current_height ||
+    height(@positions[bot] + Vector[-1, 1]) >= current_height ||
+    height(@positions[bot] + Vector[0, 1]) >= current_height ||
+    height(@positions[bot] + Vector[1, 1]) >= current_height
+  end
+
+  def raise bot
+    @field[@positions[bot][0]][@positions[bot][1]] += 1
+  end
+
+  def height position
+    @field[position[0]][@field[1]]
   end
 end
 
@@ -70,6 +118,10 @@ class Bot
 
   def dig
     game.dig self
+  end
+
+  def raise_possible?
+    game.raise_possible? self
   end
 
   def raise
