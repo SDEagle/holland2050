d6ccd3463ef6a6aec8b7aedfa823e4f7493a30d4
Tim 'S.D.Eagle' Zeitz <tim.zeitz@googlemail.com>
2013-05-31 01:11:55 +0200

    scaffolding

---
+++ main.rb
@@ -0,0 +1,76 @@
+require 'matrix'
+
+id = 0
+def get_id
+  id += 1
+end
+
+class Game
+  def initialize field_size
+    @field = []
+    @water = 0
+    @bots = []
+    @positions = {}
+    field_size.times do [i]
+      @field << []
+      field_size.times do
+        @field[i] << 3
+      end
+    end
+  end
+
+  def add_bot bot
+    @bots << bot
+    @positions[bot] = Vector[0,0,0]
+  end
+
+  def next_round
+    raise_water
+    @bots.each do |bot|
+      bot.act @water, @positions[bot], filter_sight bot, @field
+    end
+  end
+
+  def raise_water
+    @water += 1
+  end
+
+  def filter_sight bot, field
+    field
+  end
+end
+
+class Bot
+  attr_reader :id
+
+  def initialize game
+    @game = game
+    @id = get_id
+  end
+
+  def move direction
+    game.move self, direction if move_possible? direction
+  end
+
+  def move_possible? direction
+    game.move_possible? self, direction
+  end
+
+  def dig
+    game.dig self
+  end
+
+  def raise
+    game.dig self
+  end
+
+  def == other
+    return false unless other.is_a? Bot
+    @id == other.id
+  end
+
+  def hash
+    @id.hash
+  end
+end
+
