521d1e985a2b0d7107f6a9d272378c1be8f62f4a
jonathan <jonathan@thinkpad.(none)>
2013-05-31 19:35:58 +0200

    noise ... sort of

--- map.rb
+++ map.rb
@@ -1,15 +1,41 @@
 class Map
-  def initialize field_size, max_height
-    @field = []
-    field_size.times do |i|
-      @field << []
-      field_size.times do
-        @field[i] << rand(max_height)
+  attr_accessor :field
+  def initialize field_size, height
+    @field = Array.new(field_size) { Array.new(field_size) { 0 } }
+    random_field = Array.new(field_size) { Array.new(field_size) { rand height } }
+    step_one = Array.new(field_size / 5) { Array.new(field_size / 5) }
+    raster = (field_size / 5)
+    raster.times do |i|
+      raster.times do |j|
+        step_one[i][j] = random_field[i][j]
       end
     end
+
+    field_size.times do |i|
+      field_size.times do |j|
+        unless i % 5 == 0 || j % 5 == 0
+          q11 = [i, j, step_one[i/5][j/5]]
+          q12 = [i, j+5, step_one[i/5][(j+1)/5]]
+          q21 = [i+5, j, step_one[(i+1)/5][j/5]]
+          q22 = [i+5, j+5, step_one[(i+1)/5][(j+1)/5]]
+          p = [i, j, 0]
+          @field[i][j] = interpolate_bilinear q11, q12, q21, q22, p
+        end
+      end
+    end
+
+    print_field @field
   end
 
-  def noise frequency
-
+  def interpolate_bilinear q11, q12, q21, q22, p
+    r1 = (q21[0] - p[0]) / (q21[0] - q11[0]) * q11[2] + (p[0] - q11[0]) / (q21[0] - q11[0]) * q21[2]
+    r2 = (q21[0] - p[0]) / (q21[0] - q11[0]) * q12[2] + (p[0] - q11[0]) / (q21[0] - q11[0]) * q22[2]
+    (q22[1] - p[1]) / (q22[1] - q21[1]) * r1[2] + (p[1] - q11[1]) / (q22[1] - q11[1]) * r2[2]
+  end
+  
+  def print_field field
+    puts (field.map { |inner| inner.join "\s" }).join "\n"
   end
 end
+
+map = Map.new 20, 99
